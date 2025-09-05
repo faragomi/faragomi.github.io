06e0491f15f57d21a07f8bfff04a6ec1e6126f37
faragomi <faragomi@protonmail.ch>
2025-09-05 18:02:20 +0200

    Családi táblázat hozzáadása

--- script/tablazatMegjelenit.js
+++ script/tablazatMegjelenit.js
@@ -19,7 +19,37 @@ Papa.parse("adat/tarsasLista.csv",{
     header: true,
     complete: function(results){
         const adat = results.data;
-        const table = document.getElementById("tarsasjatekLista");
+        const table = document.getElementById("tarsasjatekLista-1");
+    
+    // fejlés létrehozása
+    const headerRow = document.createElement('tr');
+    Object.keys(adat[0]).forEach(key => {
+        const th = document.createElement("th");
+        th.textContent = key;
+        headerRow.appendChild(th);
+    });
+    table.appendChild(headerRow);
+
+    // adatsorok létrehozása
+    adat.forEach(row => {
+        const tr = document.createElement("tr");
+        Object.values(row).forEach(value => {
+            const td = document.createElement("td");
+            td.textContent = value;
+            tr.appendChild(td);
+        });
+        table.appendChild(tr);
+    });
+    }
+});
+
+
+Papa.parse("adat/tarsasListaKozos.csv",{
+    download: true,
+    header: true,
+    complete: function(results){
+        const adat = results.data;
+        const table = document.getElementById("tarsasjatekLista-2");
     
     // fejlés létrehozása
     const headerRow = document.createElement('tr');
